a18bc2943a488518efad074af30021e9492fd714
philiprbaumann <77937811+philiprbaumann@users.noreply.github.com>
2021-09-13 21:49:12 -0400

    one easy

---
+++ Array/maxUnitsOnTruck.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/maximum-units-on-a-truck/
+# Solved this on my own, it was an easy.
+# My approach created a new variable instead of just decreasing truckSize. 
+# This is something I need to keep in mind. Updating existing values is always going to be more efficient than allocating
+# a new variable and updating that one. 
+class Solution(object):
+    def maximumUnits(self, boxTypes, truckSize):
+        boxTypes.sort(key= lambda x: x[1], reverse=True)
+        unitCount = 0
+        
+        for numBoxes, boxUnits in boxTypes:
+            if truckSize > numBoxes:
+                truckSize -= numBoxes
+                unitCount += numBoxes * boxUnits
+            else:
+                unitCount += truckSize * boxUnits 
+                break
+        return unitCount 
+    
+        """
+        :type boxTypes: List[List[int]]
+        :type truckSize: int
+        :rtype: int
+        """
