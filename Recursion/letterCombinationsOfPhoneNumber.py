c409705d3bef6db5ab5f1f6c587cc4fdc3de56f0
philiprbaumann <77937811+philiprbaumann@users.noreply.github.com>
2021-10-05 20:23:28 -0400

    add 0 and 1 cases

--- Recursion/letterCombinationsOfPhoneNumber.py
+++ Recursion/letterCombinationsOfPhoneNumber.py
@@ -8,6 +8,8 @@ class Solution(object):
             return []
         
         letterMap = {
+            '0': [''],
+            '1': [''],
             '2': ['a', 'b', 'c'],
             '3': ['d', 'e', 'f'],
             '4': ['g', 'h', 'i'],
