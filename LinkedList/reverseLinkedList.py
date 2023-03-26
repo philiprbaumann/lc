55d93c0edb1fef00f126569b084faf8c1633940c
philiprbaumann <77937811+philiprbaumann@users.noreply.github.com>
2023-03-26 00:24:00 -0400

    Reverse linked list pt2.

--- LinkedList/reverseLinkedList.py
+++ LinkedList/reverseLinkedList.py
@@ -1,3 +1,5 @@
+# The trick with this one is remembering to return last/prev and not current because current would be equal
+# to the next of the last node ( which is null ).
 #https://leetcode.com/problems/reverse-linked-list/
 # Definition for singly-linked list.
 # class ListNode(object):
@@ -15,4 +17,23 @@ class Solution(object):
             current = next_node
         return last
             
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        prev = None
+        curr = head 
+        while (curr):
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp
             
+        return prev
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
